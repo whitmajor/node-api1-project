1c2ad34c1988a3f2c2c70cf41cee357c3b7bc8d6
whitmajor <98010797+whitmajor@users.noreply.github.com>
2022-07-12 22:33:50 -0400

    added server

--- api/server.js
+++ api/server.js
@@ -1,3 +1,13 @@
 // BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require("express")
+
+const server = express()
+
+server.use("*",(req, res) => {
+    res.status(404).json({
+        message: "not found"
+    })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
