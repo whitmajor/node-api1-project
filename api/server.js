40705259c46314782ab0b05caf3d5a5fc73e582a
whitmajor <98010797+whitmajor@users.noreply.github.com>
2022-07-12 23:29:34 -0400

    added get request for users

--- api/server.js
+++ api/server.js
@@ -2,12 +2,72 @@
 
 const express = require("express")
 
+const User = require("./users/model")
+
 const server = express()
+server.use(express.json())
+
+server.post("/api/users",(req,res)=>{
+    const user = req.body;
+    if(!user.name || !user.bio){
+        res.status(422).json({
+            message: "Please provide name and bio for the user",
+        })
+
+    }else{
+    User.insert(user)
+    .then(createUser =>{
+        res.status(201).json(createUser)
+    })
+    .catch(err =>{
+        res.status(500).json({
+            message:"error creating user",
+            err: err.message,
+            stack: err.stack,
+        })
+    })
+    }
+})
+
+server.get( "/api/users", (req, res) => {
+    User.find()
+        .then(user => {
+         res.json(user)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: "error getting errors",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+})
+
+server.get( "/api/users/:id", (req, res) => {
+    User.findById(req.params.id)
+        .then(user => {
+        if(!user){
+            res.status(404).json({
+                message: "The user with the specified ID does not exist",
+            })
+        }
+         res.json(user)
+        })
+        .catch(err => {
+            res.status(50).json({
+                message: "error getting user",
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+})
 
 server.use("*",(req, res) => {
     res.status(404).json({
         message: "not found"
     })
 })
+
+
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
